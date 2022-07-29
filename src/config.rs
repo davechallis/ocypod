5f1ad28a3b47ba141cea32ca4680e03b5f85838c
Dave Challis <dave@suicas.net>
2022-07-29 16:49:21 +0100

    Initial work on environment interpolation into configuration files (see #20).

--- src/config.rs
+++ src/config.rs
@@ -8,11 +8,15 @@ use std::str::FromStr;
 use std::collections::HashMap;
 
 use log::{debug, warn};
+use regex::Captures;
+use regex::Regex;
 use serde::de::Deserializer;
 use serde::Deserialize;
 use structopt::StructOpt;
 
 use crate::models::Duration;
+
+const INTERPOLATE_RE: &str = r"(?m)\$\{([A-Z][A-Z0-9_]*)(?:=([^}]+))?\}";
 
 /// Parsed command line options when the server application is started.
 #[derive(Debug, StructOpt)]
@@ -97,6 +101,23 @@ impl Config {
     /// Get the Redis URL to use for connecting to a Redis server.
     pub fn redis_url(&self) -> &str {
         &self.redis.url
+    }
+
+    fn interpolate_env(raw_toml: &str) -> std::borrow::Cow<str> {
+        let re = Regex::new(INTERPOLATE_RE)
+            .expect("failed to compile interpolation regex");
+
+        re.replace_all(raw_toml, |captures: &Captures| {
+            let var_name = captures.get(1)
+                .expect("capture should have at least 1 group");
+
+            let value = match std::env::var(var_name.as_str()) {
+                Ok(env_val) => env_val,
+                Err(_) => captures.get(2).map_or_else(String::new, |v| v.as_str().to_owned())
+            };
+
+            value
+        })
     }
 }
 
@@ -263,5 +284,171 @@ retry_delays = ["10s", "1m", "5m"]
         assert_eq!(q3.expires_after, Duration::from_secs(5400));
         assert_eq!(q3.retries, 4);
         assert_eq!(q3.retry_delays, vec![Duration::from_secs(10), Duration::from_secs(60), Duration::from_secs(300)]);
+    }
+
+    #[test]
+    fn interpolation_regex_no_match() {
+        let re = Regex::new(INTERPOLATE_RE).unwrap();
+        assert!(re.captures("").is_none());
+        assert!(re.captures("foo").is_none());
+        assert!(re.captures("{foo").is_none());
+        assert!(re.captures("foo}").is_none());
+        assert!(re.captures(" ").is_none());
+        assert!(re.captures("").is_none());
+        assert!(re.captures("${foo}").is_none());
+        assert!(re.captures("${Foo}").is_none());
+        assert!(re.captures("${123FOO}").is_none());
+        assert!(re.captures("${A B C} ${D E F}").is_none());
+    }
+
+    #[test]
+    fn interpolation_regex_match() {
+        let re = Regex::new(INTERPOLATE_RE).unwrap();
+        let capture = re.captures("key = ${VALUE}").unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "VALUE");
+
+        let capture = re.captures("key = ${VA_LUE}").unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "VA_LUE");
+
+        let capture = re.captures("key = ${VA_LUE_123}").unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "VA_LUE_123");
+
+        let capture = re.captures("key = ${VALUE=default}").unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "VALUE");
+        assert_eq!(capture.get(2).unwrap().as_str(), "default");
+
+        let capture = re.captures("key = ${VALUE=A longer (default) value}").unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "VALUE");
+        assert_eq!(capture.get(2).unwrap().as_str(), "A longer (default) value");
+
+        let capture = re.captures("key = \"${FOO_1=true}, ${FOO_2=1}\"").unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "FOO_1");
+        assert_eq!(capture.get(2).unwrap().as_str(), "true");
+    }
+
+    #[test]
+    fn interpolation_regex_match_multiple() {
+        let re = Regex::new(INTERPOLATE_RE).unwrap();
+        let captures: Vec<_> = re.captures_iter("${ONE=1} ${TWO=2}").into_iter().collect();
+        assert_eq!(captures.len(), 2);
+
+        let capture = captures.get(0).unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "ONE");
+        assert_eq!(capture.get(2).unwrap().as_str(), "1");
+
+        let capture = captures.get(1).unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "TWO");
+        assert_eq!(capture.get(2).unwrap().as_str(), "2");
+    }
+
+    #[test]
+    fn interpolation_regex_match_multiline() {
+        let re = Regex::new(INTERPOLATE_RE).unwrap();
+        let conf = r#"
+[server]
+host = "::1"
+port = ${OCYPOD_PORT=8023}
+log_level = "${OCYPOD_LOG_LEVEL=info}"
+
+[redis]
+url = "redis://${REDIS_HOST}:${REDIS_PORT}"
+
+[queue.default]
+
+[queue.another-queue]
+
+[queue.a_3rd_queue]
+timeout = "${DEFAULT_QUEUE_TIMEOUT}"
+heartbeat_timeout = "${DEFAULT_QUEUE_TIMEOUT}"
+expires_after = "90m"
+retries = 4
+retry_delays = ["10s", "1m", "5m"]
+        "#;
+        let captures: Vec<_> = re.captures_iter(conf).into_iter().collect();
+        assert_eq!(captures.len(), 6);
+
+        let capture = captures.get(0).unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "OCYPOD_PORT");
+        assert_eq!(capture.get(2).unwrap().as_str(), "8023");
+
+        let capture = captures.get(1).unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "OCYPOD_LOG_LEVEL");
+        assert_eq!(capture.get(2).unwrap().as_str(), "info");
+
+        let capture = captures.get(2).unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "REDIS_HOST");
+        assert!(capture.get(2).is_none());
+
+        let capture = captures.get(3).unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "REDIS_PORT");
+        assert!(capture.get(2).is_none());
+
+        let capture = captures.get(4).unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "DEFAULT_QUEUE_TIMEOUT");
+        assert!(capture.get(2).is_none());
+
+        let capture = captures.get(5).unwrap();
+        assert_eq!(capture.get(1).unwrap().as_str(), "DEFAULT_QUEUE_TIMEOUT");
+        assert!(capture.get(2).is_none());
+    }
+
+    #[test]
+    fn interpolation_from_env_defaults() {
+        let re = Regex::new(INTERPOLATE_RE).unwrap();
+        let conf = r#"
+[server]
+port = ${OCYTEST_OCYPOD_PORT=8023}
+log_level = "${OCYTEST_OCYPOD_LOG_LEVEL=info}"
+
+[redis]
+url = "redis://${OCYTEST_REDIS_HOST=localhost}:${OCYTEST_REDIS_PORT=6379}"
+
+[queue.${OCYTEST_QUEUE_PREFIX}foo]
+        "#;
+
+        let expected = r#"
+[server]
+port = 8023
+log_level = "info"
+
+[redis]
+url = "redis://localhost:6379"
+
+[queue.foo]
+        "#;
+
+        assert_eq!(Config::interpolate_env(conf), expected);
+    }
+
+    #[test]
+    fn interpolation_from_env() {
+        let re = Regex::new(INTERPOLATE_RE).unwrap();
+        std::env::set_var("OCYTEST_B_OCYPOD_LOG_LEVEL", "debug");
+        std::env::set_var("OCYTEST_B_REDIS_HOST", "example.com");
+        std::env::set_var("OCYTEST_B_QUEUE_PREFIX", "prefix_");
+
+        let conf = r#"
+[server]
+port = ${OCYTEST_B_OCYPOD_PORT=8023}
+log_level = "${OCYTEST_B_OCYPOD_LOG_LEVEL=info}"
+
+[redis]
+url = "redis://${OCYTEST_B_REDIS_HOST=localhost}:${OCYTEST_B_REDIS_PORT=6379}"
+
+[queue.${OCYTEST_B_QUEUE_PREFIX}foo]
+        "#;
+
+        let expected = r#"
+[server]
+port = 8023
+log_level = "debug"
+
+[redis]
+url = "redis://example.com:6379"
+
+[queue.prefix_foo]
+        "#;
+
+        assert_eq!(Config::interpolate_env(conf), expected);
     }
 }
